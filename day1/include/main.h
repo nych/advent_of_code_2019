52e82095139c54f763d31cbedf49fb0055e47924
Christian Nydegger <nydegger.ch@gmail.com>
2020-05-03 20:58:19 +0200

    solved day1

--- day1/include/main.h
+++ day1/include/main.h
@@ -1,1 +1,1 @@
-int main(void);
+int main(int argc, char **argv);
--- day1/src/main.c
+++ day1/src/main.c
@@ -1,7 +1,37 @@
 #include "../include/main.h"
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello world\n");
-  return 0;
+int main(int argc, char **argv) {
+	if (argc <= 1) {
+		printf("[ERROR] - Mandatory attribute 'filepath' missing");
+		return 1;
+	}
+
+	FILE *file = fopen(argv[1], "r");
+	if (file == NULL) {
+		printf("[ERROR] - Unable to open file");
+		return 1;
+	}
+
+	int totalFuelRequired = 0;
+	int bufferLength = 256;
+	char buffer[bufferLength];
+	while (fgets(buffer, bufferLength, file)) {
+		int mass = atoi(buffer);
+		int fuel;
+		do
+		{
+			fuel = fuelForMass(mass);
+			totalFuelRequired += fuel;
+			mass = fuel;
+		} while (fuel > 0);
+	}
+
+	printf("total fuel required %d\n", totalFuelRequired);
+	return 0;
+}
+
+int fuelForMass(int mass) {
+	int result = (mass / 3) - 2;
+	return result > 0 ? result : 0;
 }
